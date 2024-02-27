94464a19560bea5bd9bbc7f3b1364b8e00beb9f6
M <malekuk9@gmail.com>
2024-02-27 19:04:12 +0000

    count

--- database_connection.py
+++ database_connection.py
@@ -36,7 +36,7 @@ def db_find_group(by: telebot.types.Message):
         stmt = select(Group).where(Group.id == by.chat.id)
         matches = session.execute(stmt)
 
-        if not matches or len(matches) == 0:
+        if matches.count == 0 or matches.count > 1:
             return None
 
         return matches[0]
@@ -47,7 +47,8 @@ def db_member_exists(message: telebot.types.Message):
         stmt = select(Group).where(Group.id == message.chat.id)
         matches = session.execute(stmt)
 
-        if not matches or len(matches) == 0:
+        if matches.count > 1 or matches.count == 0:
+            return True
             return False
 
         # find a member in matches with the same id as message.from_user.id
